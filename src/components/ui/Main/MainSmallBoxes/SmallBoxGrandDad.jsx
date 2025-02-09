e878e8a225282a0857d1618899f091bcd539b039
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-09 21:52:15 +0530

    Updated SmallBoxGrandDad.jsx

--- src/components/ui/Main/MainSmallBoxes/SmallBoxGrandDad.jsx
+++ src/components/ui/Main/MainSmallBoxes/SmallBoxGrandDad.jsx
@@ -3,7 +3,7 @@ import { MainSmallBoxesParent } from "./MainSmallBoxesParent";
 
 export function SmallBoxGrandDad() {
   return (
-    <div className="flex flex-col gap-12 mt-10">
+    <div className="flex flex-col gap-12 mt-11">
       {Array.from({ length: 10 }, (_, index) => (
         <MainSmallBoxesParent key={index} />
       ))}
