bca103fcd283fb868484aa21c8a5629fe03c6d39
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-09 13:32:01 +0530

    some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import { ContextProvider } from "./components/ui/context/ContextProvider";
 function App() {
   return (
     <ContextProvider>
-      <div className="w-full h-full">
-        <div className="h-[6vh]">
+      <div className="w-full flex flex-col gap-3 h-full">
+        <div>
           <Header />
         </div>
         <div className="grid w-full gap-x-3 grid-cols-[0.7fr_2fr] xl:grid-cols-[0.6fr_2fr] md:grid-cols-[1fr_2fr] h-[93.8vh]">
