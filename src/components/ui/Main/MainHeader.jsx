0d38bc352bffae7692680d2175f3cb3ed76bbb7f
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-09 21:52:15 +0530

    Updated MainHeader.jsx

--- src/components/ui/Main/MainHeader.jsx
+++ src/components/ui/Main/MainHeader.jsx
@@ -3,10 +3,12 @@ import { useState, useEffect } from "react";
 import { Context } from "../context/Context";
 export function MainHeader() {
   const { scrolled } = useContext(Context);
+console.log(scrolled)
+
   return (
     <div
-      className={`flex gap-2 w-full ${
-        scrolled ? "fixed bg-[#221960]" : "bg-transparent "
+      className={`flex z-10 fixed gap-2 w-full ${
+        scrolled ? " bg-[#221960]" : "bg-transparent "
       } transition-all duration-700 `}
     >
       <div className="py-2 px-4 bg-gray-600/50 text-sm  cursor-pointer w-fit rounded-full text-white ">
