66310ae623a0bdaa884fefeb5886b4bab9b75a45
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-09 21:52:15 +0530

    Updated MainSmallBoxesParent.jsx

--- src/components/ui/Main/MainSmallBoxes/MainSmallBoxesParent.jsx
+++ src/components/ui/Main/MainSmallBoxes/MainSmallBoxesParent.jsx
@@ -7,17 +7,17 @@ export function MainSmallBoxesParent() {
     setWord(faker.word.words(3));
   }, []);
   return (
-    <div className="flex flex-col gap-3 max-h-fit w-full">
+    <div className="flex flex-col gap-5 max-h-fit  w-fit">
       <div className="flex justify-between items-center cursor-pointer ">
         <h2 className="text-white text-[1.9rem] hover:underline decoration-1 font-bold">
           {word}
         </h2>
-        <p className="text-white/70 hover:underline decoration-white decoration-1 ">
+        <p className="text-white mt-1 text-sm mr-11 font-semibold hover:underline decoration-white decoration-1 ">
           Show all
         </p>
       </div>
-      <div className="flex overflow-x-auto w-full gap-6 ">
-        {Array.from({ length: 12 }, (_, index) => (
+      <div className="flex overflow-x-auto max-w-[85vh]  gap-6 ">
+        {Array.from({ length: Math.floor(Math.random(5) * 50) }, (_, index) => (
           <MainSmallBoxes key={index} />
         ))}
       </div>
