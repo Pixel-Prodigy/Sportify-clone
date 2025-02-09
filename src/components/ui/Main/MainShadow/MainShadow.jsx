5fbb6498be629a52a40a3a3017ce714e51ebf414
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-09 21:52:15 +0530

    Updated MainShadow.jsx

--- src/components/ui/Main/MainShadow/MainShadow.jsx
+++ src/components/ui/Main/MainShadow/MainShadow.jsx
@@ -3,13 +3,13 @@ import { motion } from "framer-motion";
 
 export function MainShadow({ topShadow }) {
   const gradientArray = [
-    "linear-gradient(to bottom, rgba(60, 30, 15, 0.85) 20%,#121212 100%)",
-    "linear-gradient(to bottom, rgba(40, 3, 60, 0.85) 20%,#121212 100%)",
-    "linear-gradient(to bottom, rgba(60, 10, 50, 0.85) 20%,#121212 100%)",
-    "linear-gradient(to bottom, rgba(60, 20, 60, 0.85) 20%,#121212 100%)",
-    "linear-gradient(to bottom, rgba(70, 40, 40, 0.85) 20%,#121212 100%)",
-    "linear-gradient(to bottom, rgba(35, 60, 40, 0.85) 20%,#121212 100%)",
-    "linear-gradient(to bottom, rgba(40, 10, 60, 0.85) 20%,#121212 100%)",
+    "linear-gradient(to bottom, rgba(60, 30, 15, 0.75) 20%,#121212 100%)",
+    "linear-gradient(to bottom, rgba(40, 3, 60, 0.75) 20%,#121212 100%)",
+    "linear-gradient(to bottom, rgba(60,  25,25, 0.75) 20%,#121212 100%)",
+    "linear-gradient(to bottom, rgba(30, 20, 30, 0.75) 20%,#121212 100%)",
+    "linear-gradient(to bottom, rgba(60, 45, 45, 0.75) 20%,#121212 100%)",
+    "linear-gradient(to bottom, rgba(35, 60, 40, 0.75) 20%,#121212 100%)",
+    "linear-gradient(to bottom, rgba(5, 5, 90, 0.75) 20%,#121212 100%)",
   ];
 
   const [currentGradient, setCurrentGradient] = useState(gradientArray[0]);
@@ -19,12 +19,13 @@ export function MainShadow({ topShadow }) {
       const newGradient =
         gradientArray[Math.floor(Math.random() * gradientArray.length)];
       setCurrentGradient(newGradient);
+    }else{
+      setCurrentGradient("linear-gradient(to bottom , #11171f 20% , #121212 100%)")
     }
   }, [topShadow]);
 
   return (
     <div>
-      {/* Top Shadow */}
       <motion.div
         className="absolute -z-10 h-[250px] w-full top-0 left-0 transition-all"
         initial={{ opacity: 0.7 }}
@@ -36,7 +37,6 @@ export function MainShadow({ topShadow }) {
         }}
       ></motion.div>
 
-      {/* Bottom Shadow */}
       <div
         className="absolute h-[90px] w-full top-[230px] left-0"
         style={{
