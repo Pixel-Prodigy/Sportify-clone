c07d0ca5b5bd8adf9c1fdf8d169fcf91f84714df
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-09 21:52:15 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import { ContextProvider } from "./components/ui/context/ContextProvider";
 function App() {
   return (
     <ContextProvider>
-      <div className="w-full flex flex-col gap-3 h-full">
+      <div className="w-full flex flex-col px-2 gap-3 h-full">
         <div>
           <Header />
         </div>
-        <div className="grid w-full gap-x-3 grid-cols-[0.7fr_2fr] xl:grid-cols-[0.6fr_2fr] md:grid-cols-[1fr_2fr] h-[93.8vh]">
+        <div className="flex w-full gap-x-2 xl:grid-cols-[0.6fr_2fr] md:grid-cols-[1fr_2fr] h-[93.8vh]">
           <SideBar />
           <Main />
         </div>
