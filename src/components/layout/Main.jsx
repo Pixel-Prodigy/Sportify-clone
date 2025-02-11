13749b0bc4ab034cf2579159acb392d101ec7023
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-11 14:43:52 +0530

    Updated src/components/layout/Main.jsx

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,19 +1,17 @@
-import React, { useContext, useEffect } from "react";
 import { MainHeader } from "../ui/Main/MainHeader";
-import { Context } from "../ui/context/Context";
-import { HistoryParent } from "../ui/Main/mainHistory/HistoryParent";
-import { MainShadow } from "../ui/Main/MainShadow/MainShadow";
 import { MainTopComp } from "../ui/Main/MainTopComp";
-import { MainSmallBoxesParent } from "../ui/Main/MainSmallBoxes/MainSmallBoxesParent";
 import { SmallBoxGrandDad } from "../ui/Main/MainSmallBoxes/SmallBoxGrandDad";
 
 export function Main() {
   return (
-    <div className="main w-full z-1 overflow-x-scroll relative px-10 bg-[#121212] pt-5 rounded-lg shadow-xl">
-      <MainHeader />
-      <div className="pt-7 mainParent">
-        <MainTopComp />
-        <SmallBoxGrandDad />
+    <div className="main w-full z-1 overflow-y-auto relative px-10 bg-[#121212] pt-5 rounded-lg shadow-xl">
+      <div className="ml-[40%]">
+        {" "}
+        <MainHeader />
+        <div className="pt-7 max-h-[90vh] mainParent">
+          <MainTopComp />
+          <SmallBoxGrandDad />
+        </div>
       </div>
     </div>
   );
