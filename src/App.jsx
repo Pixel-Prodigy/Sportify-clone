b01cdc35c4c6eea2f6b26c793b9fb97c9ce52bc6
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-11 14:43:52 +0530

    Updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
   return (
     <ContextProvider>
       <div className="w-full flex flex-col px-2 gap-3 h-full">
-        <div>
+        <div className="h-full">
           <Header />
         </div>
-        <div className="flex w-full gap-x-2 xl:grid-cols-[0.6fr_2fr] md:grid-cols-[1fr_2fr] h-[93.8vh]">
+        <div className="flex w-full gap-x-2 xl:grid-cols-[0.6fr_2fr] md:grid-cols-[1fr_2fr] ">
           <SideBar />
           <Main />
         </div>
